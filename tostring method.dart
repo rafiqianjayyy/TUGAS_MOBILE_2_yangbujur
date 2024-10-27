7628fff5ddb4c281318447a535b2fa4410f80555
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-27 21:49:53 +0700

    Create tostring method.dart

---
+++ tostring method.dart
@@ -0,0 +1,19 @@
+import '../data/productstring.dart';
+
+void main() {
+  var product = Product();
+  print(product.toString());
+  print(product);
+}
+
+//contoh
+// import '../data/productstring.dart';
+
+// void main() {
+//   var product = Product();
+//   product.id = '1';
+//   product.name = 'Laptop';
+
+//   print(product.toString());
+//   print(product);
+// }
