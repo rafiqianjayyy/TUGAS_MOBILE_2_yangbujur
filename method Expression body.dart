1f79e251d33cdebd41a030cde33ddf313f0567ce
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-27 21:10:15 +0700

    Create method Expression body.dart

---
+++ method Expression body.dart
@@ -0,0 +1,31 @@
+// class Computer {
+//   // void startup() {
+//   //   print("computer is starting");
+//   // }
+
+//   void startup() => print("computer is starting");
+
+//   void shutdown() => print("computer is shutting down");
+
+//   String getOperatingSystem() => "Linux";
+// }
+
+//Ini Memanggil Method Expression Body
+class Computer {
+  void startup() {
+     print("computer is starting");
+   }
+
+  void startup() => print("computer is starting");
+
+  void shutdown() => print("computer is shutting down");
+
+  String getOperatingSystem() => "Linux";
+}
+
+void main() {
+  var computer = Computer();
+  computer.startup();
+  computer.shutdown();
+  print(computer.getOperatingSystem());
+}
