23636a541311648625a32a743738f851af85ba56
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-27 21:50:36 +0700

    Create equals operator.dart

---
+++ equals operator.dart
@@ -0,0 +1,9 @@
+import '../data/categoryequalsoperator.dart';
+
+void main() {
+  var category1 = Category("1", "Laptop");
+  var category2 = Category("1", "Laptop");
+
+  print(category1 == category1);
+  print(category1 == category2);
+}
