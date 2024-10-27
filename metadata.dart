a95e25b2fb31eb5c19d0e8a471397899b4b521c5
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-27 21:59:59 +0700

    Create metadata.dart

---
+++ metadata.dart
@@ -0,0 +1,21 @@
+// //Ini Standard Annotation
+// class Sample {
+//   @override //Menandai bahwa field atau method merupakan overriding dari parentnya
+//   String toString() {
+//     return "Sample";
+//   }
+
+//   @Deprecated("Don't use it anymore") //Menandai bahwa kode sudah tidak direkomendasikan untuk digunakan
+//   void doNotCallMe() {}
+// }
+
+// //Ini Membuat Annotation
+// class Todo {
+//   final String todo;
+//   const Todo(this.todo);
+// }
+
+// class ApplicationLogic{
+//   @Todo("Will be implemented in next feature")
+//   void run(){}
+// }
