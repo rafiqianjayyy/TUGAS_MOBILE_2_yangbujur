a01701f8dd8a001da69b1591ca0d5c584a2f565a
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-27 21:59:13 +0700

    Create error.dart

---
+++ error.dart
@@ -0,0 +1,15 @@
+// //Error
+void main() {
+  var list = ["RAFIQI", "ANJAYY", "."];
+  print(list[10]);
+}
+
+
+//Ini Tidak Error
+// void main() {
+//   var list = ["RAFIQI", "ANJAYY", "."];
+
+//   if (list.length > 10) {
+//     print(list[10]);
+//   }
+// }
